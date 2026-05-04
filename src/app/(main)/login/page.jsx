14ac7152f1b7706049aa1b1ac9cae89bd066d370
jonayet-nur <jonayetnur@gmail.com>
2026-05-04 21:50:00 +0600

    responsive problem fixed and final deploy

--- src/app/(main)/login/page.jsx
+++ src/app/(main)/login/page.jsx
@@ -49,7 +49,7 @@ export default function LoginInPage() {
   };
 
   return (
-    <Card className="border mx-auto w-125 py-10 my-5">
+    <Card className="border w-full mx-auto lg:w-125 py-10 my-5">
       <h1 className="text-center text-2xl font-bold">Login</h1>
 
       <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
